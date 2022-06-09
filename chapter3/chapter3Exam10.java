d8f2b01ab7e689b2fb8a4fcc9c881dd89904124e
moonjulee4327 <mjlee4327@gmail.com>
2022-06-10 00:19:51 +0900

    new

--- chapter3/chapter3Exam10.java
+++ chapter3/chapter3Exam10.java
@@ -7,12 +7,9 @@ public class chapter3Exam10 {
 		int var2 = 3;
 		int var3 = 14;
 		
-		char cVar2 = (char) var2;
-		char cVar3 = (char) var3;
-		double dvar2 = (double) cVar2;
-		double dvar3 = (double) cVar3;
 		
-		double var4 = (double)var1 * var1 * (cVar2 + '.' + cVar3);
+		
+		double var4 = var1 * var1 * Double.parseDouble(var2 + "." + var3);
 		System.out.println("원의 넓이:" + var4);
 
 	}
