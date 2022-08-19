4b79d9184c9e6c1b958d02ba69d132c270999de7
moonjulee4327 <mjlee4327@gmail.com>
2022-08-19 21:09:40 +0900

    secondStep

---
+++ Java/secondStep/JavaNetworkTest/src/http/MyHttpServer.java
@@ -0,0 +1,15 @@
+package http;
+
+/*
+ * 간단한 웹서버 예제
+ * @author pc17
+ * */
+public class MyHttpServer {
+	private final int port = 80;
+	private final String encoding = "UTF-8";
+	
+	// http서버 시작...
+	public void start() {
+		
+	}
+}
