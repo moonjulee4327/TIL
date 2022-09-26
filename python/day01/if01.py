19756a07fca492b3b368c9ee04fef351cfc4af8f
moonjulee4327 <mjlee4327@gmail.com>
2022-09-26 20:47:57 +0900

    new

---
+++ python/day01/if01.py
@@ -0,0 +1,23 @@
+a = True
+b = not True
+
+# if a:
+#     print("참")
+# print("참")
+# # 둘은 다른 소스다.
+# if a:
+#     print("참")
+#     print("참")
+
+#안됨(if문이 끝난것으로 인식)
+# if b:
+#     print("참")
+#     print("참")
+# print("참")
+# else:
+#     print("거짓")
+
+if b:
+    pass
+else:
+    print("거짓")
